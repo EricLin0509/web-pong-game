1cea58238465b890ff3d2f31f813d873bed80a0e
EricLin0509 <ericlin050914@gmail.com>
2026-06-16 17:28:04 +0800

    [Enhancement] Setup service worker caching

--- web-pong-game/vite.config.js
+++ web-pong-game/vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,33 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      scope: '/web-pong-game/',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4096 * 1024, // 4 MB
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wasm}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.(wasm)$/, // Cache wasm files
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'wasm-cache',
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 30, // Update every 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/.*\.(js)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'js-cache',
+            },
+          },
+        ],
+      },
+    }),
   ],
   assetsInclude: [
     '**/*.wasm',
